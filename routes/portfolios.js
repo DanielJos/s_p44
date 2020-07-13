be03478e19552527fad89f28ea3e316e402598d4
kotysii <57452187+kotysii@users.noreply.github.com>
2020-07-13 11:42:58 +0100

    Removed duplicate Joi code, Now Validated GET PARAM and responds page title (added that doc property)

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,3 +1,4 @@
+const debug = require("debug")("p44:debug");    // debugging
 const Joi = require('@hapi/joi');
 const mongoose = require("mongoose");
 const express = require("express");
@@ -9,21 +10,33 @@ router.use(express.static("./stylesheets"))
 
 // define the DB's portfolio schema
 const portfolioSchema = new mongoose.Schema({
-    portfolioID: {
+    "portID": {
         type: String,
         required: true,
         minlength: 5,
         lowercase: true,
+    },
+    "title": {
+        type: String,
+        required: true
     }
+
 });
 
-// Joi req.body schema
-const joischema = Joi.object({
-    portfolioID: Joi.string().min(5).required()
-  });
+// Joi req.body schema (for validation)
+function validatePortfolio(portfolio) {     // with Joi
 
-// define a new model
-const Portfolio = mongoose.model("Portfolio", portfolioSchema);
+const schema = Joi.object( {
+    "portID": Joi.string().min(5).required(),
+    "title": Joi.string().required()
+});
+console.log(schema.validate(portfolio));
+
+return schema.validate(portfolio);
+}  
+
+// define a new model           // collection, schema
+const Portfolio = mongoose.model("portfolio", portfolioSchema);
 
 router.get("/", async (req, res)=>{
     
@@ -32,39 +45,54 @@ router.get("/", async (req, res)=>{
 });
 
 // get a portfolio according to the end of the url
-router.get("/:portID", async (req, res)=>{
+router.get("/:pID", async (req, res)=>{
     try{                                        // TODO: need to consider validation
     // get the portfolio (await promise)
-    const portfolio = await Portfolio.find({
-        portfolioID: req.params.portID
+    const portfolio = await Portfolio.findOne({
+        portID: req.params.pID
     });
-    console.log(req.params.portID);
-    res.render("index", {username: req.params.portID});
+    
+    if(!portfolio){
+        res.status(404).send("404: PAGE NOT FOUND");
+        debug("404: PAGE NOT FOUND");
     }
-    catch{
-        console.error(error.message);
+    else{
+        res.render("index", {username: portfolio.title} );
+        debug(`Rendered: ${portfolio.portID}`)
+    }
+
+    
+    }
+    catch(err){
+        console.log(err.errors);
+        res.status(404);
     }
 });
 
 // post a portfolio from body
 router.post("/", async (req, res)=>{
-      // validate the req.body against the schema
-      const result = joischema.validate(req.body);
 
-      console.log(result);
+    // validate the req.body against the schema
+    debug(req.body);
+        try{
+            const result = validatePortfolio(req.body);
+        }
+        catch(err){
+            console.log(err.message);
+        }
+        // bail is error
+        // if(result.error){
+        //     return res.status(400).send(result.error);
+        // }
 
-      // bail is error
-      if(result.error){
-          return res.status(400).send(result.error);
-      }
+        // "let" because we want to return the ID property when it has saved
+        let portfolio = new Portfolio({     // when adding more properties remember both schema
+            portID: req.body.portID,
+            title: req.body.title
+        })
+        portfolio = await portfolio.save();
 
-      // "let" because we want to return the ID property when it has saved
-      let portfolio = new Portfolio({
-          portfolioID: req.body.portfolioID
-      })
-      portfolio = await portfolio.save();
-
-      res.send(portfolio);
+        res.send(portfolio);
 });
 
 // find a portfolio, validate and then add the addition
@@ -79,11 +107,11 @@ router.put("/:portID", async (req, res)=>{
       }
 
     // query first
-    const portfolio = await Portfolio.find( {portfolioID: portID} );
+    const portfolio = await Portfolio.find( {portID: portID} );
     // if portfolio doesnt exist
     if(!portfolio) return res.status(404).send("404");
     portfolio.set({
-        portfolioID: req.body.PortfolioID
+        portID: req.body.portID
     });
     portfolio = await portfolio.save();
     debug(result);
@@ -92,21 +120,15 @@ router.put("/:portID", async (req, res)=>{
 router.delete("/:portID", async (req, res)=>{
 
     // find portfolio
-    const portfolio = await Portfolio.find( {portfolioID: portID} );
+    const portfolio = await Portfolio.find( {portID: portID} );
     if(!portfolio) return res.status(404).send("No Portfolio to Delete...");
 
     // delete the portfolio
-    const result = await Portfolio.deleteOne( {portfolioID: portID} );
+    const result = await Portfolio.deleteOne( {portID: portID} );
     res.send(result + portfolio);
 
 });
 
-function validatePortfolio(portfolio) {
-    const schema = {
-      portfolioID: Joi.string().min(5).required()
-    };
-  
-    return schema.validate(portfolio);
-  }
+
 
 module.exports = router;
